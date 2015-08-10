9e711876084e0dad2f1f78e92f640c2d35e9a13b
paldepind <simonfv@gmail.com>
2015-08-10 09:33:03 +0200

    Remove unused variables #53

--- flyd.js
+++ flyd.js
@@ -1,4 +1,4 @@
-(function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.flyd = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+!function(e){if("object"==typeof exports&&"undefined"!=typeof module)module.exports=e();else if("function"==typeof define&&define.amd)define([],e);else{var f;"undefined"!=typeof window?f=window:"undefined"!=typeof global?f=global:"undefined"!=typeof self&&(f=self),f.flyd=e()}}(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 var curryN = require('ramda/src/curryN');
 
 'use strict';
@@ -160,12 +160,8 @@ function markListeners(s, lists) {
 
 function createStream() {
   function s(n) {
-    if (arguments.length === 0) {
-      return s.val;
-    } else {
-      updateStreamValue(s, n);
-      return s;
-    }
+    return arguments.length > 0 ? (updateStreamValue(s, n), s)
+                                : s.val;
   }
   s.hasVal = false;
   s.val = undefined;
@@ -189,7 +185,7 @@ function addListeners(deps, s) {
 }
 
 function createDependentStream(deps, fn) {
-  var i, s = createStream();
+  var s = createStream();
   s.fn = fn;
   s.deps = deps;
   s.depsMet = false;
