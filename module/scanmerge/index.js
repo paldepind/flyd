9b8d080d5404b0247ba3699405f64f08957ab9d9
Dante Federici <c.dante.federici@gmail.com>
2015-11-26 00:19:29 -0500

    Updated scanMerge to new API. Passing tests now.

--- module/scanmerge/index.js
+++ module/scanmerge/index.js
@@ -1,13 +1,25 @@
-var flyd = require('flyd');
+var flyd = require('../../lib');
 
 module.exports = flyd.curryN(2, function(pairs, acc) {
-  var streams = pairs.map(function(p) { return p[0]; });
-  var fns = pairs.map(function(p) { return p[1]; });
-  return flyd.stream(streams, function(self, changed) {
-    if (changed.length > 0) {
-      var idx = streams.indexOf(changed[0]);
-      acc = fns[idx](acc, changed[0]());
-    }
-    return acc;
-  }, true);
+  // The scanMerged stream and accumulator
+  var scanMerged = flyd.stream();
+  
+  // update scanMerged with any function/stream pair
+  // @todo: make atomic currently in order of register in scanMerged.
+  var onStream = flyd.on(function(valAndFn){
+    scanMerged(valAndFn[1](scanMerged(), valAndFn[0]));
+  });
+  
+  var streamsWithFn = pairs.map(function(streamAndFn) {
+    return streamAndFn[0].map(function(x) {
+      return [x, streamAndFn[1]];
+    });
+  });
+  
+  streamsWithFn.map(onStream);
+  
+  // Enforce initial value
+  scanMerged(acc);
+  
+  return scanMerged;
 });
