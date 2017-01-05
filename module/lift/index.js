4d396b6f76edcca250feb6e5567196032be474f8
Fabian D <fabian.dubois@gmail.com>
2017-01-05 18:28:42 +0900

    Make lift update on undefined

--- module/lift/index.js
+++ module/lift/index.js
@@ -4,7 +4,8 @@ module.exports = function(f /* , streams */) {
   var streams = Array.prototype.slice.call(arguments, 1);
   var vals = [];
   return flyd.combine(function() {
+    var self = arguments[arguments.length - 2];
     for (var i = 0; i < streams.length; ++i) vals[i] = streams[i]();
-    return f.apply(null, vals);
+    self(f.apply(null, vals));
   }, streams);
 };
