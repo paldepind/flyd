b8b00da88c117d022d592b591cf65d378a339b03
William Esz <flagello@users.noreply.github.com>
2017-04-30 07:36:16 +0200

    Adopt Rollup entirely, drop Browserify

--- rollup.config.js
+++ rollup.config.js
@@ -3,8 +3,8 @@ import resolve from 'rollup-plugin-node-resolve';
 export default {
   entry: 'lib/index.js',
   dest: 'flyd.js',
-  moduleName: '@most/prelude',
-  format: 'cjs',
+  format: 'umd',
+  moduleName: "flyd",
   plugins: [
     resolve({
       modulesOnly: true
