6f715c44f5bd3a4acd92851dc7e2ae33807494fe
Einar Norðfjörð <einar@vortex.is>
2018-02-01 21:36:21 +0000

    actually implement the Applicative specification

--- test/index.js
+++ test/index.js
@@ -702,6 +702,14 @@ describe('stream', function() {
   });
 
   describe('of', function() {
+    it('can be accessed through the constructor property', function() {
+      var s1 = stream(2);
+      var s2 = s1.constructor.of(3);
+      var s3 = s2.constructor['fantasy-land/of'](3);
+      assert.equal(flyd.isStream(s2), true);
+      assert.equal(s2(), 3);
+      assert.equal(s3(), 3);
+    });
     it('returns a stream with the passed value', function() {
       var s1 = stream(2);
       var s2 = s1.of(3);
