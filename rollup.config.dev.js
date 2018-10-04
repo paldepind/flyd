f29b3d24147580851d6b22121912f25656a6f0c3
futurist <26634873@qq.com>
2018-10-04 22:56:42 +0800

    treat ramda as external in ES bundle

--- rollup.config.dev.js
+++ rollup.config.dev.js
@@ -1,7 +1,8 @@
 import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
+import replace from 'rollup-plugin-replace';
 
-export default {
+export default [{
   input: './lib/index.js',
   output: {
     file: './flyd.js',
@@ -12,4 +13,19 @@ export default {
     resolve(),
     commonjs()
   ]
-};
+}, {
+  input: './lib/index.js',
+  output: {
+    file: './es/index.js',
+    format: 'es'
+  },
+  external: ['ramda/src/curryN'],
+  plugins: [
+    commonjs(),
+    // below can be optimized after the rollup issue resolved
+    // https://github.com/rollup/rollup/issues/2491
+    replace({
+      'ramda/src/curryN': 'ramda/es/curryN'
+    })
+  ]
+}];
