7e5a6b6c554028a5ea4a2989701f5fcccd5f62aa
Einar Norðfjörð <gloogankle@gmail.com>
2024-02-05 15:10:54 -0500

    simplify test

--- test/delayed-stream-end.js
+++ test/delayed-stream-end.js
@@ -1,40 +1,26 @@
-var assert = require('assert');
-var R = require('ramda');
+var assert = require("assert");
+var R = require("ramda");
 
-var flyd = require('../lib');
+var flyd = require("../lib");
 
-function once(s) {
-  return flyd.combine(function(s, self) {
-    self(s.val)
-    self.end(true)
-  }, [s])
-}
-function withLatestFrom() {
-  var streams = arguments
-  return flyd.combine(function() {
-    var self = arguments[arguments.length - 2]
-    var result = []
-    for (var i = 0; i < streams.length; ++i) {
-      if (!streams[i].hasVal) return
-      result.push(streams[i].val)
-    }
-    self(result)
-  }, streams)
-}
-
-describe('ending a stream', function() {
-  it('delays ending the current stream until dependents have been updated', function() {
-    var stream = flyd.stream(1)
-    function doubled() { return stream.map(R.multiply(2)) }
+describe("ending a stream", function () {
+  it("delays ending the current stream until dependents have been updated", function () {
+    var stream = flyd.stream(1);
     var count = 0;
-    stream
-      .map(function() {
-        withLatestFrom(doubled(), doubled())
-          .pipe(once)
-          .map(function() {
-            count++
-          })
-      })
-    assert.equal(count, 1)
-  })
-})
+    stream.map(function () {
+      var s1 = stream.map(R.add(1));
+      var s2 = stream.map(R.add(2));
+      flyd
+        .combine(
+          function (s1, s2, self) {
+            self(s1() + s2());
+            self.end(true);
+          },
+          [s1, s2],
+        )
+        // was not called prior to #229
+        .map(function () { count++ });
+    });
+    assert.equal(count, 1);
+  });
+});
